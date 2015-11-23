ef347e5995d19779f1074afa467ae0fbf45bb119
Niels van Schooten <nielsvanschooten@gmail.com>
2015-11-23 20:38:48 +0100

    Added the new CSharp version of the dataCollector.

---
+++ project/CSDataCollector/CSDataCollector/DataCollector/DataParser.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSDataCollector
+{
+    class DataParser
+    {
+
+        public Dictionary<string, List<string>> buffer { get; set; }
+
+
+        public DataParser()
+        {
+
+        }
+    }
+}
---
+++ project/CSDataCollector/CSDataCollector/DataCollector/Inputmanager.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Net;
+using uPLibrary.Networking.M2Mqtt;
+using uPLibrary.Networking.M2Mqtt.Messages;
+
+namespace DataCollector.Input
+{
+
+    public class InputManager
+    {
+
+        private MqttClient Client { get; set; }
+
+        private string[] subjects = new string[4] {"Connection", "Event", "Monitoring", "Position" };
+
+        private byte[] qosLevels = { MqttMsgBase.QOS_LEVEL_AT_MOST_ONCE, MqttMsgBase.QOS_LEVEL_AT_MOST_ONCE, MqttMsgBase.QOS_LEVEL_AT_MOST_ONCE, MqttMsgBase.QOS_LEVEL_AT_MOST_ONCE };
+
+        public InputManager(string _sAddress)
+	    {
+            Client = new MqttClient(IPAddress.Parse(_sAddress));
+            Client.Connect(Guid.NewGuid().ToString());
+
+
+            Client.Subscribe(subjects, qosLevels);
+
+            Client.MqttMsgPublishReceived += MessageReceived;
+	    }
+
+        public void MessageReceived(object sender, MqttMsgPublishEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+
+
+    }
+}
